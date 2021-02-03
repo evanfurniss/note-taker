56ca51a5a8307ce135a57b9a8676bfca62c02cef
Evan Furniss <evanfurniss@gmail.com>
2021-02-02 18:57:20 -0600

    Added get method for the notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -10,8 +10,12 @@ app.use(express.json());
 var notes = [];
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./"))
-})
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 app.post("/api/notes", function (req, res){
     var newNote = req.body.textarea;
