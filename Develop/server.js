34a5bfda1f25e32445c7b2b2748d307af6da429a
Evan Furniss <evanfurniss@gmail.com>
2021-02-03 15:54:31 -0600

    Switching machines

--- Develop/server.js
+++ Develop/server.js
@@ -1,42 +1,32 @@
 // Set up initial methods, server call, and PORT number
 const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
 var app = express();
 var PORT = 4200;
-
 //=====================================================================================================
 // Set up recieving and sending note data, switches from json to note data, and notes arr to store notes
-
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-
 var notes = [];
-
 //=====================================================================================================
 // When called, retrieves relevant data for the call
-
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-
+app.get("/api/notes", function(req, res) {
+})
 app.post("/api/notes", (req, res) => {
     var newNote = req.body.textarea;
-
     newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
-
     notes.push(newNote);
-
     res.json(newNote);
 });
-
 //=====================================================================================================
 // Listener that takes ajax calls and retrieves and sends back data
-
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
