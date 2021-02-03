5021f4b2e4575ce6c1e8beff735d2329c98b4c8d
Evan Furniss <evanfurniss@gmail.com>
2021-02-02 19:06:31 -0600

    added clarifying notes for myself, switched post statement to ES6

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,20 @@
+// Set up initial methods, server call, and PORT number
 const express = require("express");
 const path = require("path");
 
 var app = express();
 var PORT = 4200;
 
+//=====================================================================================================
+// Set up recieving and sending note data, switches from json to note data, and notes arr to store notes
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 var notes = [];
+
+//=====================================================================================================
+// When called, retrieves relevant data for the call
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -17,7 +24,7 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.post("/api/notes", function (req, res){
+app.post("/api/notes", (req, res) => {
     var newNote = req.body.textarea;
 
     newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
@@ -26,6 +33,9 @@ app.post("/api/notes", function (req, res){
 
     res.json(newNote);
 });
+
+//=====================================================================================================
+// Listener that takes ajax calls and retrieves and sends back data
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
