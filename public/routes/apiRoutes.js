537e1b75e8794bba95c3959640f68a8fc7015531
Evan Furniss <evanfurniss@gmail.com>
2021-02-03 18:03:16 -0600

    trying again, my commits did not seem to stick

--- public/routes/apiRoutes.js
+++ public/routes/apiRoutes.js
@@ -1,8 +1,8 @@
-
+var notes = require("../data/notes");
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-        
+        res.json(notes);
     });
     app.post("/api/notes", (req, res) => {
         var newNote = req.body;
--- public/routes/htmlRoutes.js
+++ public/routes/htmlRoutes.js
@@ -7,4 +7,7 @@ module.exports = function (app) {
     app.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, "../html/notes.html"));
     });
-}
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../html/index.html"));
+    });
+};
