c69ec5960e47cad03940578c178ef85c5d6c8eef
Evan Furniss <evanfurniss@gmail.com>
2021-02-04 10:19:47 -0600

    Able to get my notes to populate on the left column

--- public/apiRoutes.js
+++ public/apiRoutes.js
@@ -1,40 +1,46 @@
 var fs = require("fs");
 var util = require("util");
+var path = require("path");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-        readFileAsync(__dirname + "/db/db.json", "utf8", function(data){
-            var notes = [].concat(JSON.parse(data))
-            res.json(notes);
+        console.log("get route hit");
+        readFileAsync(path.join(__dirname + "/db/db.json"), "utf8") 
+        .then(function(data){
+            notes=[].concat(JSON.parse(data))
+            console.log(data);
+            return res.json(notes);
         })
     });
 
     app.post("/api/notes", (req, res) => {
         var newNote = req.body;
-        readFileAsync(__dirname + "/db/db.json", "utf8", function (notes) {
-            var notes = [].concat(JSON.parse(notes));
+        readFileAsync(path.join(__dirname + "/db/db.json"), "utf8") 
+        .then(function (data) {
+            notes = [].concat(JSON.parse(data));
             newNote.id = notes.length + 1;
+            // console.log(notes);
             notes.push(newNote);
-            console.log(newNote);
             return notes
-        }).then(function(notes){
-            writeFileAsync(__dirname + "/db/db.json", JSON.stringify(notes))
+        }).then(function(data){
+        writeFileAsync(path.join(__dirname + "/db/db.json"), JSON.stringify(data))
             res.json(newNote);
         })
     })
 
     app.delete("/api/notes/:id"), function(req, res){
         var noteToDelete = req.params.id;
-        readFileAsync(__dirname + "/db/db.json", "utf8").then(function(note){
-            const notes = [].concat(JSON.parse(note));
+        readFileAsync(path.join(__dirname + "/db/db.json"), "utf8")
+        .then(function(data){
+            notes = [].concat(JSON.parse(data));
             const newNotes = [];
             notes.forEach((note) => noteToDelete !== note.id ? newNotes.push(note) : console.log("sorry chump"));
             return newNotes
         }).then(function (notes){
-            writeFileAsync(__dirname + "/db/db.json", JSON.stringify(notes))
+            writeFileAsync(path.join(__dirname + "/db/db.json"), JSON.stringify(notes))
             res.send("saved");
         })
     };
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 // Set up initial methods, server call, and PORT number
+
 const express = require("express");
+
 var app = express();
 var PORT = process.env.PORT || 4200;
+
 //=====================================================================================================
 // Set up recieving and sending note data, switches from json to note data, and notes arr to store notes
-app.use(express.urlencoded({ extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// For static middleware (css file etc)
+
 app.use(express.static('public'))
 
 //=====================================================================================================
 // When called, retrieves relevant data for the call
-require("./public/htmlRoutes")(app);
+
 require("./public/apiRoutes")(app);
+require("./public/htmlRoutes")(app);
+
 //=====================================================================================================
 // Listener that takes ajax calls and retrieves and sends back data
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
